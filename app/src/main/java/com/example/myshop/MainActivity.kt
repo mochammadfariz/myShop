6534e7100b15cc5d0c4938084def2c0eea1704a4
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-10 07:32:34 +0700

    adding rv in mainavctivity

--- app/src/main/java/com/example/myshop/MainActivity.kt
+++ app/src/main/java/com/example/myshop/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.myshop
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.LinearLayout
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        
+
+        recyler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 }
